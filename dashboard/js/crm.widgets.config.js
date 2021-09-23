496db4643072cfdb862f3dc3ce5c26536e849efd
ksplash38 <omegajnr38@gmail.com>
2021-09-23 01:30:08 +0100

    final push

--- dashboard/js/crm.widgets.config.js
+++
@@ -1,1 +0,0 @@
-const CrmWidgetsConfigModule=angular.module('prf.widgets.config',[]);const EnvConfig={api:'https://api.investcentral.proftit.com/',streamer:'https://streamer.investcentral.proftit.com/',requestCache:'true',};CrmWidgetsConfigModule.constant('prfEnvConfig',EnvConfig);
